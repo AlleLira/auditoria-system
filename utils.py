93e1771df8d347fd5bd3015ae7ed63b6c31344e4
Alessandra Lira <105083697+AlleLira@users.noreply.github.com>
2026-05-29 13:59:24 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,91 +1,163 @@
 from supabase_client import supabase
-import streamlit as st
 
 import pandas as pd
+import streamlit as st
+
 
 def carregar_produtos():
+
     dados = (
+
         supabase
+
         .table("produtos")
+
         .select("*")
+
         .order("nome_produto")
+
         .execute()
+
     )
 
     return [
+
         x["nome_produto"]
+
         for x in dados.data
+
     ]
+
 
 def carregar_pendencias():
 
     retorno = (
+
         supabase
+
         .table("pendencias")
+
         .select("*")
+
         .execute()
+
     )
 
     return pd.DataFrame(
+
         retorno.data
+
     )
+
 
 def salvar_pendencia(dados):
 
-    supabase.table("pendencias").insert(dados).execute()
+    supabase.table(
+
+        "pendencias"
+
+    ).insert(
+
+        dados
+
+    ).execute()
 
 
 def atualizar_pendencia(
-        id_pendencia,
-        dados
+
+    id_pendencia,
+
+    dados
+
 ):
-    
+
     (
+
         supabase
+
         .table("pendencias")
+
         .update(dados)
+
         .eq(
+
             "id",
+
             id_pendencia
+
         )
+
         .execute()
+
     )
+
 
 def excluir_pendencia(id_p):
 
     (
+
         supabase
+
         .table("pendencias")
+
         .delete()
+
         .eq(
+
             "id",
+
             id_p
+
         )
+
         .execute()
+
     )
 
+
 def salvar_entrada_produto(dados):
+
     (
-        supabase.table(
+
+        supabase
+
+        .table(
+
             "entrada_produtos"
-        ).insert(
+
+        )
+
+        .insert(
+
             dados
-        ).execute()
+
+        )
+
+        .execute()
+
     )
+
 
 def buscar_encomenda_dav(dav):
 
     response = (
 
-        supabase.table(
+        supabase
+
+        .table(
+
             "entrada_produtos"
+
         )
 
         .select("*")
 
         .eq(
+
             "dav",
+
             dav
+
         )
 
         .execute()
@@ -93,6 +165,7 @@ def buscar_encomenda_dav(dav):
     )
 
     return response.data
+
 
 def buscar_produto_codigo(codigo):
 
@@ -108,18 +181,27 @@ def buscar_produto_codigo(codigo):
     response = (
 
         supabase
+
         .table(
+
             "codigos_produto"
+
         )
 
         .select(
+
             "nome"
+
         )
 
         .filter(
+
             "codigo",
+
             "eq",
+
             codigo
+
         )
 
         .execute()
@@ -127,15 +209,7 @@ def buscar_produto_codigo(codigo):
     )
 
 
-    print(response.data)
-
-
-    if response.data and len(response.data) > 0:
-
-        return response.data[0]["nome"]
-
-
-    return ""
+    if response.data:
 
         return response.data[0]["nome"]
 
